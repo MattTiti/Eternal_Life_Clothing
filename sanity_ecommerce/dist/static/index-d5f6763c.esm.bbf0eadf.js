70e527c0c5da98dbe127ec869de81aae76c9de5e
MattTiti <mtiti3@vt.edu>
2023-01-07 18:56:01 -0500

    work

---
+++ sanity_ecommerce/dist/static/index-d5f6763c.esm.bbf0eadf.js
@@ -0,0 +1,1 @@
+import{a as t,D as m}from"./index-ae1970a3.esm.2c69d0cd.js";import"./sanity.9b4e9c69.js";import"./index.e9dc394e.js";export{t as DocumentPaneProvider,m as default};
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'bf7uy16o',
+    dataset: 'production'
+  }
+})
